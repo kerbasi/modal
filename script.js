96851b97d7439227f14706d02c30b3859f8f15f2
Kirill <kirill.novikov@gmial.com>
2023-03-16 08:56:42 +0300

    Add listeners for open btns, overlay and close btn

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+const buttons = document.querySelectorAll('.show-modal');
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+
+const toggleModal = () => {
+  modalEl.classList.toggle('hidden');
+  overlayEl.classList.toggle('hidden');
+};
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    toggleModal();
+  });
+});
+
+overlayEl.addEventListener('click', () => {
+  toggleModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  toggleModal();
+});
